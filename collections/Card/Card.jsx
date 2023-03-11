a1d2ead484d88477093308f27bd57395025730ce
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-11 20:26:24 +0200

    Update Card.jsx

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,8 @@
+import { StyledSectionBigHeading, StyledSectionTinyHeading } from "../../components/Typography/elements";
+
 // The Card to be exported goes here
+export const Card = ({children}) => {
+    return (
+     <StyledSectionTinyHeading>{children}</StyledSectionTinyHeading>
+    );
+  };
