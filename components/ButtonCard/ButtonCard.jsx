885a9124cbf6e28f6f8b31e42cb8ccab48568cc9
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-12 16:42:44 +0200

    Create button card

---
+++ components/ButtonCard/ButtonCard.jsx
@@ -0,0 +1,8 @@
+import { StyledButtonCard } from "./elements";
+
+export const ButtonCard = ({ children,...props }) => {
+  
+    return (
+     <StyledButtonCard>{children}</StyledButtonCard>
+    );
+  };
