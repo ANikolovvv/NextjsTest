9ad19f7c41e1642affac169ecff84ed53772788b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-12 16:45:44 +0200

    Update Agency

--- sections/Agency/Agency.jsx
+++ sections/Agency/Agency.jsx
@@ -1,8 +1,46 @@
 import { SectionAgencySmallTitle } from "../../components/Typography/SectionAgencySmallTitle";
 import { SectionAgencyTitle } from "../../components/Typography/SectionAgencyTitle";
-import { Container, HeaderBox } from "./elements";
+import { CardsBox, Container, HeaderBox, ImageContainer } from "./elements";
+import Image from "next/dist/client/image";
+import { Card } from "../../collections/Card/Card";
+import { BiNews, BiGlobe, BiPackage } from "react-icons/bi";
+import { ButtonCard } from "../../components/ButtonCard";
+import { StyledCardParagraph } from "../../collections/Card/elements";
 
 export const Agency = () => {
+  const cardProps = [
+    {
+      title: "Brief",
+      description: (
+        <StyledCardParagraph>
+          Complete <b>brief writing or simple guidance</b> on what to include,
+          we've got you covered.
+        </StyledCardParagraph>
+      ),
+      icon: <BiNews color="black" size={140} />,
+    },
+    {
+      title: "Search",
+      description: (
+        <StyledCardParagraph>
+          In depth agency search covering; <b>criteria matching,</b> door
+          knocking and duo-dilligence veting.
+        </StyledCardParagraph>
+      ),
+      icon: <BiGlobe color="black" size={140} />,
+    },
+    {
+      title: "Pitch",
+      description: (
+        <StyledCardParagraph>
+          Comprehencive <b>pitch management</b>, including comms, diary
+          management and pitch hosting.
+        </StyledCardParagraph>
+      ),
+      icon: <BiPackage color="black" size={140} />,
+    },
+  ];
+
   return (
     <>
       <HeaderBox>
@@ -12,7 +50,25 @@ export const Agency = () => {
         </SectionAgencySmallTitle>
       </HeaderBox>
       <Container image={"/img/background.png"}>
-     
+        <ImageContainer>
+          <Image
+            src={"/img/video.png"}
+            width={400}
+            alt="video"
+            height={650}
+          ></Image>
+        </ImageContainer>
+        <CardsBox>
+          {cardProps.map((card) => (
+            <ButtonCard key={card.title}>
+              <Card
+                title={card.title}
+                desc={card.description}
+                icon={card.icon}
+              ></Card>
+            </ButtonCard>
+          ))}
+        </CardsBox>
       </Container>
     </>
   );
--- sections/Agency/elements.jsx
+++ sections/Agency/elements.jsx
@@ -3,20 +3,38 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  align-items: center;
   justify-content: center;
-  gap: 1px;
+  gap: 80px;
   background-image: url(${(props) => props.image});
   background-size: contain;
   background-repeat: no-repeat;
   width: 100%;
-  height: 70vh;
+  min-height: 70vh;
+  margin: 0px 40px;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 `;
 export const HeaderBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  gap: 1px;
-  width: 100%;
+  gap: 2px;
+`;
+export const CardsBox = styled.div`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  text-align: left;
+  gap: 20px;
+`;
+export const ImageContainer = styled.div`
+  height: 700px;
 `;
