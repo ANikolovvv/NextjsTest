cb7c9d5c311697814e68872a83f00a71d95926b1
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-12 16:41:45 +0200

    Update Card

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,8 +1,17 @@
-import { StyledSectionBigHeading, StyledSectionTinyHeading } from "../../components/Typography/elements";
+
+import { SectionCardTitle } from "../../components/Typography/SectionCardTitle";
+import { StyledCardBox, StyledCardsBoxTitle } from "./elements";
 
 // The Card to be exported goes here
-export const Card = ({children}) => {
-    return (
-     <StyledSectionTinyHeading>{children}</StyledSectionTinyHeading>
-    );
-  };
+export const Card = ({ children, title, desc, icon }) => {
+  console.log(title);
+  return (
+    <StyledCardBox>
+      {icon}
+      <StyledCardsBoxTitle>
+        <SectionCardTitle>{title}</SectionCardTitle>
+       {desc}
+      </StyledCardsBoxTitle>
+    </StyledCardBox>
+  );
+};
