ecdc911d3454d6b46e1cf5dd93b4bea855046662
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-12 16:43:19 +0200

    Create button card elements

---
+++ components/ButtonCard/elements.jsx
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+export const StyledButtonCard = styled.button`
+  font-family: sans-serif;
+  overflow: hidden;
+  text-align: center;
+  align-self: left;
+  display: flex;
+  align-items: left;
+  justify-content: left;
+  border-radius: 7px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+
+  min-height: 70px;
+  padding: 18px;
+  cursor: pointer;
+  border: 1px solid;
+  color: white;
+
+  &:nth-child(1) {
+    max-width: 510px;
+  }
+
+  &:nth-child(2) {
+    max-width: 540px;
+  }
+
+  &:nth-child(3) {
+    max-width: 570px;
+  }
+  &:hover {
+    border: 1px solid #03a9f4;
+  }
+`;
