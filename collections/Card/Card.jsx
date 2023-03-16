9172068dd478989f4fa7614ec3bc0e9bb42017ea
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-16 20:03:59 +0200

    Update Card.jsx

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -3,14 +3,15 @@ import { SectionCardTitle } from "../../components/Typography/SectionCardTitle";
 import { StyledCardBox, StyledCardsBoxTitle } from "./elements";
 
 // The Card to be exported goes here
-export const Card = ({ children, title, desc, icon }) => {
+export const Card = ({ children, title,  icon }) => {
 
   return (
     <StyledCardBox>
       {icon}
       <StyledCardsBoxTitle>
-        <SectionCardTitle>{title}</SectionCardTitle>
-       {desc}
+        <SectionCardTitle  href={title}
+     target="_blank">{title}</SectionCardTitle>
+       {children}
       </StyledCardsBoxTitle>
     </StyledCardBox>
   );
