807d8ae4becfaf0cac53b7e0618f62a29fe6ef46
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-03-16 20:04:15 +0200

    Update elements.jsx

--- sections/Agency/elements.jsx
+++ sections/Agency/elements.jsx
@@ -1,22 +1,36 @@
 import styled from "styled-components";
+
+export const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 0px;
+  padding: 0px;
+ 
+`;
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
-  gap: 80px;
+  gap: 60px;
   background-image: url(${(props) => props.image});
   background-size: contain;
   background-repeat: no-repeat;
   width: 100%;
   min-height: 70vh;
   margin: 0px 40px;
+  padding: 0px;
 
   @media (max-width: 1024px) {
     flex-direction: column;
     justify-content: center;
     align-items: center;
     text-align: center;
+    margin: 0px;
+    padding: 0px;
   }
 `;
 export const HeaderBox = styled.div`
@@ -37,4 +51,9 @@ export const CardsBox = styled.div`
 `;
 export const ImageContainer = styled.div`
   height: 700px;
+  @media (max-width: 1024px) {
+    margin: 0px;
+    padding: 0px;
+    width: 100%;
+  }
 `;
